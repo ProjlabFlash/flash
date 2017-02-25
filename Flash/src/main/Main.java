3a1400377ecd57b73ad9ef760d4181dbb73e1cfd
LZ <stuser@iit.bme.hu>
2017-02-25 16:21:48 +0100

    init

--- Flash/src/main/Main.java
+++ Flash/src/main/Main.java
@@ -2,12 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		//Ez egy komment, ami csak kiegészítés, ezért nem jelent konfliktust
-		
-		String[] names = {"Tim", "Koni", "Lajos, Doma"}; //Doma belépett a csapatba, ez nem jelent konfliktust :)
-		
-		for(String name : names) { //ez majd konfliktust jelent.
-			System.out.println(name + ", Welcome to Flash!");
-		}
+		System.out.print("Hello world!");
 	}
 }
