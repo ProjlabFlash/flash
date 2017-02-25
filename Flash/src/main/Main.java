e4f16d5f9804c84da8750cb287043892e1b81d89
Doma <jozsa.domi@gmail.com>
2017-02-25 15:26:14 +0100

    Kezdet 1

--- Flash/src/main/Main.java
+++ Flash/src/main/Main.java
@@ -3,5 +3,11 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Flash!");
+		
+		String[] names = {"Tim", "Koni", "Lajos"};
+		
+		for(int i = 0; i < names.length; i++) {
+			System.out.println(names[i] + ", Welcome to Flash!");
+		}
 	}
 }
