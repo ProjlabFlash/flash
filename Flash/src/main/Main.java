51cbb3d16931bfa0640f3d4d216b95e02a301fe2
LZ <stuser@iit.bme.hu>
2017-02-25 16:12:09 +0100

    LZ egy sort kitörölt

--- Flash/src/main/Main.java
+++ Flash/src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		//Ez egy komment, ami csak kiegészítés, ezért nem jelent konfliktust
-		System.out.println("Hello Flash!");
 		
 		String[] names = {"Tim", "Koni", "Lajos, Doma"}; //Doma belépett a csapatba, ez nem jelent konfliktust :)
 		
