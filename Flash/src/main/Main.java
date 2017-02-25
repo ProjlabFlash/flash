33bcb6c89673fb3c8430f95fc5bb87e5884d4aee
Doma <jozsa.domi@gmail.com>
2017-02-25 15:46:54 +0100

    Doma belép a csapatba.

--- Flash/src/main/Main.java
+++ Flash/src/main/Main.java
@@ -2,9 +2,10 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
+		//Ez egy komment, ami csak kiegészítés, ezért nem jelent konfliktust
 		System.out.println("Hello Flash!");
 		
-		String[] names = {"Tim", "Koni", "Lajos"};
+		String[] names = {"Tim", "Koni", "Lajos, Doma"}; //Doma belépett a csapatba, ez nem jelent konfliktust :)
 		
 		for(int i = 0; i < names.length; i++) {
 			System.out.println(names[i] + ", Welcome to Flash!");
