9f8cb772bc4e1a39ff3267d59ad1a070b48ca8b6
Tim <faragotimike@gmail.com>
2017-03-20 01:07:06 +0100

    Locomotive loggerrel

--- Flash/src/main/Locomotive.java
+++ Flash/src/main/Locomotive.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("unused")
 public class Locomotive extends MovingObject {
 	
@@ -11,13 +13,27 @@ public class Locomotive extends MovingObject {
 	}
 	
 	public void move() {
+		//logger enter
+		Application.logger.enter(this,"move",null);
+		
 		Railway nextRailwaySegment = CurrentRailwaySegment.next(PreviousRailwaySegment);
 		step(nextRailwaySegment);
+		
+		//logger exit
+		Application.logger.exit("");
 	}
 	
 	public void ArrivedAtStation(Station station) {
+		//logger enter
+		ArrayList<Object> paramlist=new ArrayList<Object>();
+		paramlist.add(station);
+		Application.logger.enter(this,"ArrivedAtStation", paramlist);
+		
 		if(Pulls.colorCheck(station) == true) {
 			Application.win();
 		}
+		
+		//logger exit
+		Application.logger.exit("");
 	}
 }
