83cf980fbdc8776a216bb7f93834d3e4bf68fc7c
Doma <jozsa.domi@gmail.com>
2017-03-20 02:12:39 +0100

    Switch kommentek kész

--- Flash/src/main/Switch.java
+++ Flash/src/main/Switch.java
@@ -5,10 +5,20 @@ public class Switch extends Railway {
 
 	private Railway CurrentStanding;
 	
+	/**
+	 * Konstruktor. A paraméterül kapott kimenetet állítja be a váltónak.
+	 * Null-esetén a lehetõség közül az elsõt állítja be.
+	 * @param defaultStanding A váltó kezdeti állása.
+	 * @param previousRailway Elõzõ sín. A konstruáláshoz kell.
+	 */
 	public Switch(Railway defaultStanding, Railway previousRailway) {
 		super(previousRailway);
 	}
 	
+	/**
+	 * Vált a paraméterül kapott sínre.
+	 * @param nextStanding A kapott sín.
+	 */
 	public void switchTo(Railway nextStanding) {
 		
 	}
