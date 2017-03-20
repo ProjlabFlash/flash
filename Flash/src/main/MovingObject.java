5f8df2dd76f930464624b8dc26433d164c8b4bea
Tim <faragotimike@gmail.com>
2017-03-20 01:18:54 +0100

    MovingObject loggerrel

--- Flash/src/main/MovingObject.java
+++ Flash/src/main/MovingObject.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 public abstract class MovingObject extends MetaData {
 	
 	protected Railway CurrentRailwaySegment;
@@ -11,12 +13,28 @@ public abstract class MovingObject extends MetaData {
 	}
 	
 	public void step(Railway toHere) {
+		//logger enter
+		ArrayList<Object> paramlist=new ArrayList<Object>();
+		paramlist.add(toHere);
+		Application.logger.enter(this, "step", paramlist);
+		
+		
 		CurrentRailwaySegment.setOnMe(null);
 		toHere.setOnMe(this);
 		if(Pulls != null) Pulls.step(CurrentRailwaySegment);
+		
+		
+		//logger exit
+		Application.logger.exit("");
 	}
 	
 	public void crash() {
+		//logger enter
+		Application.logger.enter(this, "crash", null);
+		
 		Application.lose();
+		
+		//logger exit
+		Application.logger.exit("");
 	}
 }
